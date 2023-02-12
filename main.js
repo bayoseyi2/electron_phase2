372529867a47cb1f2d8705f202b2b0939c468c26
bayoseyi2 <beelala789@hotmail.co.uk>
2023-02-12 11:53:19 -0400

    menu and sub-menu

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, Menu, ipcMain, BrowserWindow } = require('electron');
+const menu = require('./menu');
 
+Menu.setApplicationMenu(menu)
 
 const createWindow = () =>{
  const win = new BrowserWindow(
---
+++ menu.js
@@ -0,0 +1,32 @@
+const { Menu } = require('electron')
+const isMac = process.platform==="darwin"
+const customMenu = [
+    {
+        label: 'File',
+        submenu:[
+            {label: 'Video',
+                submenu: [{label:'Load....'}]},
+            {type:'separator'},
+            isMac ? {role: 'close'} : {role:'quit'}
+        ]
+
+       
+    },
+    {
+        label: 'Developer',
+        submenu:[
+            {role:'toggleDevTools'}
+        ]
+    }
+];
+
+if(isMac){
+    customMenu.unshift({
+        label:"",
+        submenu:[
+            {role:'quit'}
+        ]
+    })
+}
+
+module.exports = Menu.buildFromTemplate(customMenu);
